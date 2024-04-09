8453a1e076ef5548c304548b3f40a7fab40e7ba0
Olena Tr <elenaprim70@gmail.com>
2024-04-09 11:57:04 +0300

    Add project files.

---
+++ ClassTasks.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0406Practice
+{
+    public class EvenNum
+    {
+        private int Value { get; }
+        public EvenNum(int value)
+        {
+            Value = (value % 2 == 0) ? value : -1;
+
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+
+    }
+    public class NotEvenNum
+    {
+        private int Value { get; }
+        public NotEvenNum(int value)
+        {
+            Value = (value % 2 != 0) ? value : -1;
+
+        }
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    public class Triangl
+    {
+        private int _angle1 { get; set; }
+        private int _angle2 { get; set; }
+        private int _angle3 { get; set; }
+        private int _side1 { get; set; }
+        private int _side2 { get; set; }
+        private int _side3 { get; set; }
+
+        public Triangl()
+        {
+            _angle1 = 0;
+            _angle2 = 0;
+            _angle3 = 0;
+            _side1 = 0;
+            _side2 = 0;
+            _side3 = 0;
+        }
+        public Triangl(int angle1, int angle2, int angle3, int side1, int side2, int side3)
+        {
+            _angle1 = angle1;
+            _angle2 = angle2;
+            _angle3 = angle3;
+            _side1 = side1;
+            _side2 = side2;
+            _side3 = side3;
+        }
+
+        public void ShowTriangl()
+        {
+            Console.WriteLine("Parameters are not specified !");
+        }
+
+        public void ShowTriangl(int angle1, int angle2, int angle3, int side1, int side2, int side3)
+        {
+            Console.WriteLine($"Angle 1 :{_angle1}; Angle 2 :{_angle2}; Angle 3 :{_angle3};");
+            Console.WriteLine($"Side 1:{_side1},Side 2:{_side2}; Side 3:{_side3}");
+        }
+
+    }
+    public class PseudoText
+    {
+        private string UserLetters1 { get; set; }
+        private string UserLetters2 { get; set; }
+
+        private int NumbersWords { get; set; }
+
+
+        public PseudoText(string userLetters1, string userLetters2, int numbersWords)
+        {
+            UserLetters1 = userLetters1;
+            UserLetters2 = userLetters2;
+            NumbersWords = numbersWords;
+
+        }
+
+        public int RandLengthWord()
+        {
+
+            Random rnd = new Random();
+
+            return rnd.Next(3, 10);
+        }
+
+        public int RandIndex(int end)
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(0, end);
+        }
+
+        public char[] RandomChar()
+        {
+            char[] arr1and2 = new char[UserLetters1.Length + UserLetters2.Length];
+
+            char[] charArray1 = UserLetters1.ToCharArray();
+            char[] charArray2 = UserLetters2.ToCharArray();
+
+            Random rnd = new Random();
+
+            Array.Copy(charArray1, 0, arr1and2, 0, charArray1.Length);
+            Array.Copy(charArray2, 0, arr1and2, charArray1.Length, charArray2.Length);
+
+
+            return arr1and2;
+        }
+
+        public string BildSentence()
+        {
+            List<string> arrWords = new List<string>();
+
+            Random rnd = new Random();
+
+            int num = NumbersWords;
+
+            while (num > 0)
+            {
+                int length = rnd.Next(3, 9);
+
+                char[] arrWord = new char[length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    Random rnd2 = new Random();
+
+                    int index = rnd.Next(0, UserLetters1.Length + UserLetters2.Length);
+
+                    char[] arr = new char[(UserLetters1.Length + UserLetters2.Length)];
+                    arr = RandomChar();
+                    arrWord[i] = arr[index];
+                }
+
+                arrWords.Add(new string(arrWord));
+
+                num--;
+
+            }
+
+            string sentence = string.Join(" ", arrWords) + ".";
+
+            string sentenceNew = char.ToUpper(sentence[0]) + sentence.Substring(1);
+
+
+            return sentenceNew;
+
+        }
+
+        public void ShowSentence()
+        {
+            Console.WriteLine($"New sentence: {BildSentence()}");
+        }
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0406Practice
+{
+    internal class Program
+    {
+        public static void GuessNum()
+        {
+            Console.WriteLine("Enter start diapason:");
+            int startDiapason = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter end diapason:");
+            int endDiapason = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(startDiapason, endDiapason + 1);
+
+            int userChoice = 0;
+
+            while (userChoice != 3)
+            {
+                Console.WriteLine($"My number:{randomNumber}");
+
+                Console.WriteLine("More 1, less 2, you guessed !: 3 ");
+                userChoice = int.Parse(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 1:
+                        startDiapason = randomNumber;
+                        randomNumber = rnd.Next(startDiapason, endDiapason + 1);
+                        break;
+                    case 2:
+                        endDiapason = randomNumber;
+                        randomNumber = rnd.Next(startDiapason, endDiapason + 1);
+                        break;
+                    case 3:
+                        Console.WriteLine("Game over! You guessed ! ");
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect choice, try again! ");
+                        break;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even number: ");
+
+            EvenNum num = new EvenNum(4);
+            Console.WriteLine(num);
+
+            Console.WriteLine("The game guess the number: ");
+            GuessNum();
+
+            Triangl tr1 = new Triangl(90, 45, 45, 20, 10, 10);
+
+            tr1.ShowTriangl(90, 45, 45, 20, 10, 10);
+
+            PseudoText text1 = new PseudoText("aeuioj", "qwertplkhgfdszxcvbnm", 7);
+
+            text1.ShowSentence();
+
+            Console.ReadLine();
+        }
+    }
+
+    
+}
